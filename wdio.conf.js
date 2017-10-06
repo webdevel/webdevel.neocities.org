fe6e4bede8dbc1b0f3c0a45a175d9c54b7326d46
Steven Garcia <webwhammy@gmail.com>
2017-10-05 20:58:09 -0700

    add initial wdio test

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './tests'
+        './test/wdio/*[sS]pec.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -32,20 +32,44 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 3,
+    maxInstances: 5,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [{
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
+    capabilities: [
+      {
         maxInstances: 1,
-        //
-        browserName: 'firefox'
-    }],
+        browserName: 'firefox',
+        version: '56.0',
+        platform: 'Windows 10'
+      },
+      {
+        maxInstances: 1,
+        browserName: 'chrome',
+        version: '61.0',
+        platform: 'Windows 10'
+      },
+      {
+        maxInstances: 1,
+        browserName: 'internet explorer',
+        version: '11.103',
+        platform: 'Windows 10'
+      },
+      {
+        maxInstances: 1,
+        browserName: 'MicrosoftEdge',
+        version: '14.14393',
+        platform: 'Windows 10'
+      },
+      {
+        maxInstances: 1,
+        browserName: 'safari',
+        version: '10.0',
+        platform: 'macOS 10.12'
+      },
+    ],
     //
     // ===================
     // Test Configurations
@@ -75,7 +99,7 @@ exports.config = {
     baseUrl: 'http://127.0.0.1:8080',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 180000,
     //
     // Default timeout in milliseconds for request
     // if Selenium Grid doesn't send response
@@ -128,25 +152,16 @@ exports.config = {
     //
     // Make sure you have the wdio adapter package for the specific framework installed
     // before running any tests.
-    framework: 'jasmine',
+    framework: 'mocha',
     //
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/testrunner/reporters.html
     // reporters: ['dot'],
     //
-    // Options to be passed to Jasmine.
-    jasmineNodeOpts: {
-        //
-        // Jasmine default timeout
-        defaultTimeoutInterval: 10000,
-        //
-        // The Jasmine framework allows interception of each assertion in order to log the state of the application
-        // or website depending on the result. For example, it is pretty handy to take a screenshot every time
-        // an assertion fails.
-        expectationResultHandler: function(passed, assertion) {
-            // do something
-        }
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 180000
     },
 
     //
@@ -179,9 +194,12 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
-    // before: function (capabilities, specs) {
-    // },
-    //
+     before: function (capabilities, specs) {
+       let chai = require('chai');
+       global.expect = chai.expect;
+       chai.Should();
+     },
+
     /**
      * Hook that gets executed before the suite starts
      * @param {Object} suite suite details
